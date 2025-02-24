ac0af6a3619dd7bb70a0eb2bc4ed143d17930569
Zdenek ULRYCH <ulrychz@office365.zcu.cz>
2025-02-24 16:34:50 +0100

    Přidejte soubory projektu.

---
+++ Po1450_Evidence/Pages/EvidenceZisku.razor.cs
@@ -0,0 +1,10 @@
+﻿namespace Po1450_Evidence.Pages
+{
+	public partial class EvidenceZisku
+	{
+		#region Vlastnosti
+		public List<Models.Polozka> Polozky { get; private set; } = new List<Models.Polozka>();
+		private Models.Polozka Polozka { get; set; } = new Models.Polozka();
+		#endregion
+	}
+}
